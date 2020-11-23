79643264cebe2c57b9728eaaa386d867a9a92ada
zhangsan <zhangsan@cncs.com>
2020-11-23 21:01:04 +0800

    添加一些Vue.js高级部分的演示代码

---
+++ hello-vue/src/user/loginForm.js
@@ -0,0 +1,10 @@
+const loginForm = {
+    //template内只能有一个根标签
+    template: `
+        <form id="loginForm">
+            用&ensp;户&ensp;名：<input type="text"></br>
+            密&emsp;&emsp;码：<input type="password"></br>
+            <input type="button" value="登录">
+        </form>
+    `
+}
